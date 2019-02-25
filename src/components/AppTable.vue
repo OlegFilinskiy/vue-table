8f4fa1f5685f38524f8819df5ec2cc4c7eae8101
Filinskiy Oleg <filinskiy@gmarkg.com>
2019-02-25 11:40:23 +0200

    Setup pagination, add actions

--- src/components/AppTable.vue
+++ src/components/AppTable.vue
@@ -11,38 +11,66 @@
           <span class="arrow" :class="sortOrders[index] > 0 ? 'asc' : 'dsc'">
             </span>
         </th>
-        <th>Select</th>
+        <th>Actions</th>
       </tr>
       </thead>
       <tbody>
       <tr v-for="(row, index) in filteredRows">
-        <td>{{ index + 1 }}</td>
-        <td v-for="cell in row">
-          {{ cell }}
+        <td>{{ index + (pageNumber * 10) + 1 }}</td>
+        <td v-for="(cell, key) in row">
+          <span v-if="!isEdit">{{ cell }}</span>
+          <input type="text"
+                 class="input-edit"
+                 :name="key"
+                 :value="cell"
+                 v-if="isEdit">
         </td>
-        <td>
+        <td class="actions">
           <button type="button"
+                  v-if="!isEdit"
                   class="btn btn-warning btn-sm btn-edit"
                   @click="onEdit(index)"
-          >Edit</button>
+          >Edit
+          </button>
+          <button type="button"
+                  v-if="isEdit"
+                  class="btn btn-success btn-sm btn-edit"
+                  @click="onSave(index)"
+          >Save
+          </button>
+          <button type="button"
+                  class="btn btn-danger btn-sm btn-edit"
+                  @click="onDelete(index)"
+          >Delete
+          </button>
         </td>
       </tr>
       </tbody>
     </table>
-    <p class="text-center mt-3">Page {{pageNumber + 1}} of {{pageCount}}</p>
-    <div class="d-flex justify-content-center mt-3">
-      <button
-          class="btn btn-primary mr-2"
+
+    <div class="pagin">
+      <div class="text-center mt-3">Page {{pageNumber + 1}} of {{pageCount}}</div>
+      <div class="d-flex justify-content-center align-content-center mt-3">
+        <button
+          class="btn btn-primary mr-2 btn-nav"
           :disabled="pageNumber === 0"
           @click="prevPage">
-        Previous
-      </button>
-      <button
+          Previous
+        </button>
+        <ul class="pagin__list">
+          <li class="pagin__item"
+              v-for="(page, index) in pages"
+          >
+            <a href="#" @click.prevent="changePage(index)">{{ page }}</a>
+          </li>
+        </ul>
+        <button
           class="btn btn-primary btn-nav"
           :disabled="pageNumber >= pageCount -1"
           @click="nextPage">
-        Next
-      </button>
+          Next
+        </button>
+      </div>
     </div>
   </div>
 </template>
@@ -64,14 +92,16 @@
       return {
         sortKey: '',
         sortOrders: sortOrders,
-        pageNumber: 0
+        pageNumber: 0,
+        pageRange: 2,
+        isEdit: false
       }
     },
     computed: {
-      pageCount(){
+      pageCount() {
         let l = this.rows.length;
         let s = this.perPage;
-        return Math.floor(l/s);
+        return Math.floor(l / s);
       },
       filteredRows() {
         let start = this.pageNumber * this.perPage;
@@ -96,6 +126,21 @@
           }).slice(start, end)
         }
         return rows.slice(start, end)
+      },
+      rangeStart() {
+        let start = this.pageNumber - this.pageRange;
+        return (start > 0) ? start : 1;
+      },
+      rangeEnd() {
+        let end = this.pageNumber + this.pageRange;
+        return (end < this.pageCount) ? end : this.pageCount;
+      },
+      pages() {
+        let pages = [];
+        for (let i = this.rangeStart; i <= this.rangeEnd; i++) {
+          pages.push(i);
+        }
+        return pages
       }
     },
     methods: {
@@ -110,8 +155,20 @@
       prevPage() {
         this.pageNumber--;
       },
+      changePage(index) {
+        this.pageNumber = index;
+      },
       onEdit(index) {
-        console.log(index);
+        this.isEdit = !this.isEdit;
+        console.log("onEdit", index);
+      },
+      onSave(index) {
+        this.isEdit = !this.isEdit;
+        console.log("onSave", index);
+      },
+      onDelete(index) {
+        console.log("onDelete", index);
+        this.rows.splice(index, 1)
       }
     }
   }
@@ -121,44 +178,87 @@
   table
     width: 100%
 
-  th
-    background: #e9e9e9
-    cursor: pointer
-    white-space: nowrap
+    th
+      background: #e9e9e9
+      cursor: pointer
+      white-space: nowrap
 
-    &.active
-      .arrow
-        opacity: 1
+      &.active
+        .arrow
+          opacity: 1
 
-  td, th
-    border-right: 1px solid #dee2e6
-    &:first-child
-      border-left: 1px solid #dee2e6
-  tr
-    &:last-child
-      td
-        border-bottom: 1px solid #dee2e6
+    td, th
+      border-right: 1px solid #dee2e6
+      max-width: 80px
 
-  .arrow
-    display: inline-block
-    vertical-align: middle
-    width: 0
-    height: 0
-    margin-left: 5px
-    opacity: 0.66
+      &:first-child
+        border-left: 1px solid #dee2e6
 
-  .arrow
-    &.asc
-      border-left: 4px solid transparent
-      border-right: 4px solid transparent
-      border-bottom: 4px solid #000
+    tr
+      &:last-child
+        td
+          border-bottom: 1px solid #dee2e6
 
-  .arrow
-    &.dsc
-      border-left: 4px solid transparent
-      border-right: 4px solid transparent
-      border-top: 4px solid #000
+    .arrow
+      display: inline-block
+      vertical-align: middle
+      width: 0
+      height: 0
+      margin-left: 5px
+      opacity: 0.66
+
+    .arrow
+      &.asc
+        border-left: 4px solid transparent
+        border-right: 4px solid transparent
+        border-bottom: 4px solid #000
+
+    .arrow
+      &.dsc
+        border-left: 4px solid transparent
+        border-right: 4px solid transparent
+        border-top: 4px solid #000
+
+    .actions
+      white-space: nowrap
+
+      .btn
+        font-size: 10px
+
+    .input-edit
+      max-width: 73px
+      padding: 2px
+
+  .pagin
+    &__list
+      display: flex
+      justify-content: center
+      align-items: center
+      list-style: none
+      margin: 0 20px
+      padding: 0
+      @media (max-width: 568px)
+        margin: 0 10px
+
+    &__item
+      a
+        display: block
+        padding: 0 10px
+        border-radius: 4px
+        text-decoration: none
+        color: #999
+        margin: 0 6px
+        background: transparent
+        transition: all .2s
+        border: 1px solid #ccc
+        &:hover
+          border-color: #007bff
+          color: #007bff
 
   .btn-nav
     width: 100px
+    @media (max-width: 568px)
+      width: 60px
+      font-size: 12px
+      padding: 0
 </style>
